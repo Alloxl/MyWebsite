5556bc28d1094728c911f2f7faf7a80a9442ac27
PreAdam <83419217+AloneTeamDiscordBots@users.noreply.github.com>
2022-08-25 17:16:00 +0300

    Update contact.jsx

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -20,12 +20,6 @@ export default function Home() {
                         <p className="font-semibold text-xl">@efx.kjq</p>
                     </div>
                 </a>
-                //<a target="_blank" rel="norefferer" href="https://facebook.com/umutxyp/">
-                   // <div className="mt-2 flex justify-between bg-zinc-500/5 hover:bg-zinc-500/10 transition-all duration-200 px-4 py-2 rounded-lg items-center">
-                      //  <i className="fab fa-facebook fa-2x" />
-                       // <p className="font-semibold text-xl">@umutxyp</p>
-                   // </div>
-               // </a>
                 <a target="_blank" rel="norefferer" href="https://discord.com/users/766202953312108554">
                     <div className="mt-2 flex justify-between bg-zinc-500/5 hover:bg-zinc-500/10 transition-all duration-200 px-4 py-2 rounded-lg items-center">
                         <i className="fab fa-discord fa-2x" />
