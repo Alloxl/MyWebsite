3ed6ea8a3ad44a6e12c22f0f9b5380907378ed98
PreAdam <83419217+AloneTeamDiscordBots@users.noreply.github.com>
2022-11-13 13:34:41 +0300

    Update contact.jsx

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -14,16 +14,16 @@ export default function Home() {
         <p className="text-xl text-white/50 font-normal text-center mb-5">Other addresses where you can contact me.</p>
         <div className=" flex justify-center flex-col items-center">
             <div className="max-w-lg w-full">
-                <a target="_blank" rel="norefferer" href=" https://instagram.com/efx.kjq/">
+                <a target="_blank" rel="norefferer" href=" https://instagram.com/efx.mkv/">
                     <div className="flex justify-between bg-zinc-500/5 hover:bg-zinc-500/10 transition-all duration-200 px-4 py-2 rounded-lg items-center">
                         <i className="fab fa-instagram fa-2x" />
-                        <p className="font-semibold text-xl">@efx.kjq</p>
+                        <p className="font-semibold text-xl">@efx.mkv</p>
                     </div>
                 </a>
-                <a target="_blank" rel="norefferer" href="https://discord.com/users/766202953312108554">
+                <a target="_blank" rel="norefferer" href="https://discord.com/users/715575125319090217">
                     <div className="mt-2 flex justify-between bg-zinc-500/5 hover:bg-zinc-500/10 transition-all duration-200 px-4 py-2 rounded-lg items-center">
                         <i className="fab fa-discord fa-2x" />
-                        <p className="font-semibold text-xl">efx#1860</p>
+                        <p className="font-semibold text-xl">Alloxxa#4162</p>
                     </div>
                 </a>
                 <a href="mailto:avastudios@yaani.com">
